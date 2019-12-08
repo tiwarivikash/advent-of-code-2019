0e21c6caad8fca1cd2efeab8646099106a3891ec
Vikash Tiwari <tiwari.vikash@gmail.com>
2019-12-08 12:42:33 -0500

    polishing

--- src/main/java/com/github/tiwarivikash/Day2.java
+++ src/main/java/com/github/tiwarivikash/Day2.java
@@ -48,6 +48,9 @@ enum Opcode implements IntBinaryOperator {
  */
 public class Day2 {
 
+    private static final int MIN = 0;
+    private static final int MAX = 99;
+
     private static List<Integer> buildInputList(String input) {
         List<String> inputStrList = Arrays.asList(input.split(","));
         return inputStrList.stream().map(Integer::valueOf).collect(Collectors.toList());
@@ -78,16 +81,14 @@ public class Day2 {
     /**
      * Problem 2.
      */
-    private static Optional<Output> findNounAndVerb(String input, int output) {
-        final List<Integer> inputList = buildInputList(input);
-        int min = 0, max = 99;
-        for (int noun = min; noun <= max; noun++) {
-            for (int verb = min; verb <= max; verb++) {
+    private static Optional<Output> findNounAndVerb(List<Integer> inputList, int expectedOutput) {
+        for (int noun = MIN; noun <= MAX; noun++) {
+            for (int verb = MIN; verb <= MAX; verb++) {
                 List<Integer> tempList = new ArrayList<>(inputList);
                 tempList.set(1, noun);
                 tempList.set(2, verb);
                 List<Integer> result = processOpcode(tempList);
-                if (result.get(0) == output) {
+                if (result.get(0) == expectedOutput) {
                     System.out.println("Final Sequence: " + generateOutput(result));
                     return Optional.of(new Output(tempList.get(1), tempList.get(2)));
                 }
@@ -97,17 +98,20 @@ public class Day2 {
     }
 
     public static void main(String[] args) {
+        final String inputString = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,13,19,2,9,19,23,1,23,6,27,1,13,27,31,1,31,10,35,1,9,35,39,1,39,9,43,2,6,43,47,1,47,5,51,2,10,51,55,1,6,55,59,2,13,59,63,2,13,63,67,1,6,67,71,1,71,5,75,2,75,6,79,1,5,79,83,1,83,6,87,2,10,87,91,1,9,91,95,1,6,95,99,1,99,6,103,2,103,9,107,2,107,10,111,1,5,111,115,1,115,6,119,2,6,119,123,1,10,123,127,1,127,5,131,1,131,2,135,1,135,5,0,99,2,0,14,0";
+
         // Problem 1
-        String input1 = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,13,19,2,9,19,23,1,23,6,27,1,13,27,31,1,31,10,35,1,9,35,39,1,39,9,43,2,6,43,47,1,47,5,51,2,10,51,55,1,6,55,59,2,13,59,63,2,13,63,67,1,6,67,71,1,71,5,75,2,75,6,79,1,5,79,83,1,83,6,87,2,10,87,91,1,9,91,95,1,6,95,99,1,99,6,103,2,103,9,107,2,107,10,111,1,5,111,115,1,115,6,119,2,6,119,123,1,10,123,127,1,127,5,131,1,131,2,135,1,135,5,0,99,2,0,14,0";
-        List<Integer> result = processOpcode(buildInputList(input1));
+        List<Integer> inputList1 = buildInputList(inputString);
+        inputList1.set(1, 12);
+        inputList1.set(2, 2);
+        List<Integer> result = processOpcode(inputList1);
         System.out.println("Answer1: " + result.get(0));
 
         //Problem 2
-        String input2 = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,13,19,2,9,19,23,1,23,6,27,1,13,27,31,1,31,10,35,1,9,35,39,1,39,9,43,2,6,43,47,1,47,5,51,2,10,51,55,1,6,55,59,2,13,59,63,2,13,63,67,1,6,67,71,1,71,5,75,2,75,6,79,1,5,79,83,1,83,6,87,2,10,87,91,1,9,91,95,1,6,95,99,1,99,6,103,2,103,9,107,2,107,10,111,1,5,111,115,1,115,6,119,2,6,119,123,1,10,123,127,1,127,5,131,1,131,2,135,1,135,5,0,99,2,0,14,0";
-        Optional<Output> output = findNounAndVerb(input2, 19690720);
-        if (!output.isEmpty()) {
-            System.out.println("Answer2: " + (100 * output.get().noun + output.get().verb));
-        }
+        final int expectedOutput = 19690720;
+        List<Integer> inputList2 = buildInputList(inputString);
+        Optional<Output> output = findNounAndVerb(inputList2, expectedOutput);
+        output.ifPresent(value -> System.out.println("Answer2: " + (100 * value.noun + value.verb)));
     }
 
 }
